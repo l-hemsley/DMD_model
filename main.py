772d774da0a619c3312cea314ee3d48f73bf0cc4
Lizzy Hemsley <ehemsley@laas.fr>
2021-03-12 12:48:18 +0100

    added collection angle

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ class output_parameters:
         self.vector_x = self.vector_z * np.tan(self.angle_x_array_meshed)
         self.vector_y = self.vector_z * np.tan(self.angle_y_array_meshed)
         self.vector = np.array([self.vector_x, self.vector_y, self.vector_z])
+        self.effective_NA_of_vector=np.sqrt((self.angle_x_array_meshed-angle_x_centre)**2+(self.angle_y_array_meshed-self.angle_y_centre)**2)
+        self.collected_vectors=self.effective_NA_of_vector<self.half_angle
 
 def envelope_function(input, output, dmd):
     w=dmd.mirror_width
@@ -57,7 +59,7 @@ def envelope_function(input, output, dmd):
     a=input.vector
     b=output.vector
     a=np.reshape(a,(3,1,1))
-    print(c,s)
+    #print(c,s)
     diff=a-b
     f1=diff[0]*0.5*(1+c)+diff[1]*(0.5*(1-c))+diff[2]*(s/np.sqrt(2))
     f2= diff[0] *( 0.5 * (1 - c))+ diff[1] * (0.5 * (1 + c)) + diff[2] * ( - s / np.sqrt(2))
@@ -105,40 +107,50 @@ def grating_function(input, output, dmd):
 ########################## main
 
 dmd=DMD_parameters('Standard',10.8*um,0.95,np.radians(12))
-input=input_parameters(600*nm,0.05,np.radians(-8.54),np.radians(8.54),10*um)
+input=input_parameters(600*nm,0.05,np.radians(-8.54),np.radians(8.54),20*um)
 output=output_parameters(0.05,np.radians(-8.54),np.radians(8.54),200)
 envelope_function(input, output, dmd)
 
 # find orders for DMD and input
-wavelengths=np.arange(400*nm,700*nm,5*nm)
-wavelengths=np.array([700*nm])
+wavelengths=np.arange(400*nm,700*nm,1*nm)
+#wavelengths=np.array([400*nm])
+transmission=np.zeros((np.size(wavelengths)))
+transmission_collected=np.zeros((np.size(wavelengths)))
 
-for w in wavelengths:
-    input.wavelength=w
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+# fig.colorbar(im1, ax=ax1)
+# fig.colorbar(im2, ax=ax2)
+# fig.colorbar(im3, ax=ax3)
+
+for i in np.arange(np.size(wavelengths)):
+    input.wavelength=wavelengths[i]
     E_grating=grating_function(input, output, dmd)**2
     E_envelope=envelope_function(input, output, dmd)**2
     E_total=E_grating*E_envelope
+    transmission[i]=np.sum(np.sum(E_total))
+    collected_vectors = abs(output.effective_NA_of_vector) < output.half_angle
 
-#print(E_envelope)
+    E_total_collected=E_total*collected_vectors
+    transmission_collected[i] = np.sum(np.sum(E_total_collected))
 
-fig, ((ax1, ax2),(ax3,ax4)) = plt.subplots(2,2)
-#fig, (ax2) = plt.subplots(1)
-im1=ax1.imshow(E_grating,extent=(output.angle_x_array_deg[0],output.angle_x_array_deg[-1],output.angle_y_array_deg[0],output.angle_y_array_deg[-1]))
-fig.colorbar(im1,ax=ax1)
-im2=ax2.imshow(E_envelope,extent=(output.angle_x_array_deg[0],output.angle_x_array_deg[-1],output.angle_y_array_deg[0],output.angle_y_array_deg[-1]))
-fig.colorbar(im2,ax=ax2)
+    im1=ax1.imshow(E_grating,extent=(output.angle_x_array_deg[0],output.angle_x_array_deg[-1],output.angle_y_array_deg[0],output.angle_y_array_deg[-1]))
 
-im3=ax3.imshow(E_total,extent=(output.angle_x_array_deg[0],output.angle_x_array_deg[-1],output.angle_y_array_deg[0],output.angle_y_array_deg[-1]))
-fig.colorbar(im3,ax=ax3)
-
-ax1.text(output.angle_x_array_deg[0],output.angle_y_array_deg[0],'wavelength='+ str(int(input.wavelength/nm))+ 'nm, beam size='+str(input.effective_beam_size/um)+'um',color='w')
-
-#
+    im2=ax2.imshow(E_envelope,extent=(output.angle_x_array_deg[0],output.angle_x_array_deg[-1],output.angle_y_array_deg[0],output.angle_y_array_deg[-1]))
 
 
-ax1.set_title('Diffraction Orders, input ('+ str(np.degrees(input.angle_x_centre)) +','+ str(np.degrees(input.angle_y_centre))+')')
-ax2.set_title('Envelope, input ('+ str(np.degrees(input.angle_x_centre)) +','+ str(np.degrees(input.angle_y_centre))+')')
-ax3.set_title('Combined, input ('+ str(np.degrees(input.angle_x_centre)) +','+ str(np.degrees(input.angle_y_centre))+')')
+    im3=ax3.imshow(E_total,extent=(output.angle_x_array_deg[0],output.angle_x_array_deg[-1],output.angle_y_array_deg[0],output.angle_y_array_deg[-1]))
+
+    ax4.clear()
+    im4=ax4.plot(wavelengths/nm,transmission)
+    im4=ax4.plot(wavelengths/nm,transmission_collected)
+
+    ax1.text(output.angle_x_array_deg[0],output.angle_y_array_deg[0],'wavelength='+ str(int(input.wavelength/nm))+ 'nm, beam size='+str(input.effective_beam_size/um)+'um',color='w')
+
+    #
+    ax1.set_title('Diffraction Orders, input ('+ str(np.degrees(input.angle_x_centre)) +','+ str(np.degrees(input.angle_y_centre))+')')
+    ax2.set_title('Envelope, input ('+ str(np.degrees(input.angle_x_centre)) +','+ str(np.degrees(input.angle_y_centre))+')')
+    ax3.set_title('Combined, input ('+ str(np.degrees(input.angle_x_centre)) +','+ str(np.degrees(input.angle_y_centre))+')')
+    plt.pause(0.01)
 
 plt.show()
 
